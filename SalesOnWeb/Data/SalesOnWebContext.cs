003c06b61ba2c9694d26a4f6366bc6794cfff5a4
Daniel Machado <daniel.machado.av@gmail.com>
2021-02-14 13:37:38 -0300

    CRUD Scaffolding

---
+++ SalesOnWeb/Data/SalesOnWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesOnWeb.Models;
+
+namespace SalesOnWeb.Data
+{
+    public class SalesOnWebContext : DbContext
+    {
+        public SalesOnWebContext (DbContextOptions<SalesOnWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesOnWeb.Models.Department> Department { get; set; }
+    }
+}
