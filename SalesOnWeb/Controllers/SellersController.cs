6346f24d0d0bce6e68f101bf876a38277966db4f
Daniel Machado <daniel.machado.av@gmail.com>
2021-02-17 22:12:12 -0300

    Simple Create form

--- SalesOnWeb/Controllers/SellersController.cs
+++ SalesOnWeb/Controllers/SellersController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesOnWeb.Services;
+using SalesOnWeb.Models;
 
 namespace SalesOnWeb.Controllers {
     public class SellersController : Controller {
+
         private readonly SellerService _sellerService;
 
         public SellersController(SellerService sellerService) {
@@ -16,6 +18,17 @@ namespace SalesOnWeb.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll(); // << MVC happening 
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) { // MATCHING WITH THE asp-action "Create" of the Create view page in views
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
